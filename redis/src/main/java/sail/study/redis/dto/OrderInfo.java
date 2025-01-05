fd807d31be932cda8a0a19c6795f51544ad1c44b
bmsnc <qjatjs98@gmail.com>
2025-01-05 20:24:45 +0900

    Init : 프로젝트 생성 및 코드 세팅

--- redis/src/main/java/sail/study/redis/dto/OrderInfo.java
+++ redis/src/main/java/sail/study/redis/dto/OrderInfo.java
@@ -1,2 +1,15 @@
-package sail.study.redis.dto;public class OrderInfo {
+package sail.study.redis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class OrderInfo {
+
+    private String productName;
+    private Integer amount;
+    private Long timeStamp;
 }
